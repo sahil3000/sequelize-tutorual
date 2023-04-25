e2a82eeb15d5286c1a49b92fa247ce4a430b160e
Sahil Manhotra <69794254+sahil3000@users.noreply.github.com>
2023-04-25 10:38:59 +0530

    Changes

--- redisConnection.js
+++ redisConnection.js
@@ -2,11 +2,11 @@ const redis = require('redis');
 
 const client = redis.createClient({
     socket: {
-        host: 'redis-11345.c305.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: '11345'
+        host: '',
+        port: ''
     },
     username: '',
-    password: 'T8HOkS2i3xgwarD7IYnpmoM2nqE6aSRo'
+    password: ''
 });
 
 (async () => {
